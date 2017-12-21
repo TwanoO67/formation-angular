19e268b36ea0160dd034bbed1dd604acc2be7e7d
Antoine WEBER <a.weber@sdv.fr>
2017-12-21 11:09:05 +0100

    deplacement exo

--- rxjs/02-observable-from-array-items/src/app/app.component.ts
+++ rxjs/02-observable-from-array-items/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/observable';
 import 'rxjs/add/operator/map';
 import { fromEvent } from 'rxjs/observable/fromEvent';
 import { from } from 'rxjs/observable/from';
+import { Subscriber } from './subscriber';
 
 @Component({
   selector: 'app-root',
@@ -13,44 +14,42 @@ import { from } from 'rxjs/observable/from';
 export class AppComponent implements OnInit {
   monTexte = '';
 
+
+// Observable From String
+const str = 'Hello world!';
+
   // Depuis un objet
   str = 'Hello Word';
   str$ = from(this.str);
-
+  
   // Depuis un tableau
   nums: Array<number> = [33, 45, 23, 23, 4, 5];
   nums$: Observable<number> = from(this.nums);
-
+  
   // Depuis un tableau d'objet
-
   users: Array<any> = [
-    {
-      name: 'John Doe',
-      email: 'jdoe@yopmail.com'
-    },
-    {
-      name: 'User 2',
-      email: 'u2@yopmail.com'
-    }
+    { name: 'John Doe', email: 'jdoe@gmail.com' },
+    { name: 'Sam Smith', email: 'ssmith@gmail.com' },
+    { name: 'Jen Thompson', email: 'jthompson@gmail.com' }
   ];
   users$: Observable<any> = from(this.users);
-
+  
   // Idem pour les autres type de structure, Map, Set etc...
-  map: Map<number, number> = new Map([[1, 2], [2, 3]]);
+  map: Map<number, number> = new Map([[1, 2], [3, 4], [5,6]]);
   map$: Observable<any> = from(this.map);
-
-
+  
+  // Observable From Set
+  const s = new Set(['Foo', 43, { name: 'Jeff'}]);
+  const s$ = from(s);
+  
   public ngOnInit() {
 
-    this.str$.subscribe((val) => {
-      console.log('valeur recu');
-      this.monTexte = val;
-    });
-
-    setTimeout(() => {
-      console.log('nouvelle valeur');
-      this.str = 'ma2emval';
-    }, 1000 );
+    // les enregistrements
+    str$.subscribe(Subscriber.create('str'));
+    nums$.subscribe(Subscriber.create('user'));
+    users$.subscribe(Subscriber.create('user'));
+    map$.subscribe(Subscriber.create('map'));
+    s$.subscribe(Subscriber.create('set'));
 
     /*const obs: Observable<string> = from('keyup').map((e) => {
       // typescript: Property 'target' does not exist on type '{}'
